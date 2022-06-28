77ed7d0a379cba388350241769f618f8ab1d3488
AhmadAlkhadraa <ahmad.a.alkhadra@gmail.com>
2022-06-28 23:04:41 +0300

    navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './Compnent/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+    <NavbarComp/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Compnent/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Container from 'react-bootstrap/Container';
+
+const NavbarComp = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container fluid>
+    <Navbar.Brand href="#home">Ahmad Alkhadra</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#Projects">Projects</Nav.Link>
+        <Nav.Link href="#Experiences">Experiences</Nav.Link>
+        <Nav.Link href="#Education">Education</Nav.Link>
+
+      
+      </Nav>
+      <Nav>
+        <Nav.Link href="#Contact">Contact</Nav.Link>
+        <Nav.Link eventKey={2} href="#Skills">
+          Skills
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default NavbarComp
